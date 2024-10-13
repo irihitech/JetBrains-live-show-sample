84343d926b01334a6dc0133ebdb84c3a96038a9d
rabbitism <dongbin@irihi.tech>
2024-10-13 20:10:54 +0800

    feat: add unit test.

---
+++ test/Test.Todo/AddItemTest.cs
@@ -0,0 +1,24 @@
+﻿using Todo.ViewModels;
+
+namespace Test.Todo;
+
+public class AddItemTest
+{
+    [Fact]
+    public async void ExecuteCommand_WillAddNewItem()
+    {
+        // Arrange
+        var viewModel = new MainWindowViewModel();
+        Assert.Equal(9, viewModel.Items.Count);
+
+        Assert.False(viewModel.AddNewItemCommand.CanExecute(null));
+
+        viewModel.Input = "Hello World";
+
+        Assert.True(viewModel.AddNewItemCommand.CanExecute(null));
+        await viewModel.AddNewItemCommand.ExecuteAsync(null);
+        
+        Assert.Equal(10, viewModel.Items.Count);
+        Assert.Equal("Hello World", viewModel.Items.Last().Title);
+    }
+}
