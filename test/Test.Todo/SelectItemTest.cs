60db07f71fecb889a6c94320c38fba270dcce826
rabbitism <dongbin@irihi.tech>
2024-10-13 20:15:03 +0800

    feat: add second test. 增加第二个测试用例。

---
+++ test/Test.Todo/SelectItemTest.cs
@@ -0,0 +1,26 @@
+﻿using Todo.ViewModels;
+
+namespace Test.Todo;
+
+public class SelectItemTest
+{
+    [Fact]
+    public void SelectItem_Change_CloseCommandCanExecute()
+    {
+        // Arrange
+        var viewModel = new MainWindowViewModel();
+        Assert.Equal(9, viewModel.Items.Count);
+
+        Assert.False(viewModel.ClosePaneCommand.CanExecute(null));
+
+        viewModel.SelectedItem = viewModel.Items[0];
+
+        Assert.NotNull(viewModel.SelectedItem);
+
+        Assert.True(viewModel.ClosePaneCommand.CanExecute(null));
+        
+        viewModel.ClosePaneCommand.Execute(null);
+        
+        Assert.Null(viewModel.SelectedItem);
+    }
+}
